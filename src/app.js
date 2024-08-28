50869dc16399ea7e247b81b3ba59cd21626bb4a1
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-08-28 10:37:49 +0000

    Mas actualizacion

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateCardRandom() {
+    const palos = ["spade", "club", "heart", "diamond"];
+
+    const valores = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const paloRandom = palos[Math.floor(Math.random() * palos.length)];
+    const valorRandom = valores[Math.floor(Math.random() * valores.length)];
+
+    document.querySelector(".card").className = `card card-${paloRandom}`;
+    document.querySelector(
+      ".card-header"
+    ).className = `card-header ${paloRandom}`;
+    document.querySelector(".card-center").textContent = valorRandom;
+    document.querySelector(
+      ".card-footer"
+    ).className = `card-footer ${paloRandom}`;
+  }
+
+  generateCardRandom();
 };
